a270cb0f718cbe4bee7429f1a960eaf249e7c71c
tnxnpu <tinhnx@nopowerup.com>
2025-07-15 17:10:15 +0700

    run static domain ngrok

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 cd /d E:\TINHNX\PyProjects\Poll_Slack_NPU
 start "Uvicorn Server" cmd /k "cd /d E:\TINHNX\PyProjects\Poll_Slack_NPU && python -m uvicorn main:app --reload --port 8000"
 timeout /t 3 > nul
-start "ngrok Tunnel" cmd /k "ngrok http 8000"
+start "ngrok Tunnel" cmd /k "ngrok http --url=wallaby-allowing-quickly.ngrok-free.app 8000"
