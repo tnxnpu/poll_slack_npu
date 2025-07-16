15be2d7f19011069c882e4d8a3a0e51647e2703c
tnxnpu <tinhnx@nopowerup.com>
2025-07-16 10:47:47 +0700

    test

--- test.py
+++
@@ -1,9 +0,0 @@
-﻿import os
-from dotenv import load_dotenv, find_dotenv
-from settings import SLACK_BOT_TOKEN
-
-
-print(SLACK_BOT_TOKEN)
-
-
-
--- test_run_close_modal.bat
+++
@@ -1,5 +0,0 @@
-﻿@echo off
-cd /d E:\TINHNX\PyProjects\Poll_Slack_NPU
-start "Test close modal" cmd /k "cd /d E:\TINHNX\PyProjects\Poll_Slack_NPU && python -m uvicorn test_close_modal:app --reload --port 8002"
-timeout /t 3 > nul
-start "ngrok Tunnel" cmd /k "ngrok http 8002"
